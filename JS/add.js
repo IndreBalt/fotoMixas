5b133c3864a95197066077ddc733b830c14b7ae5
IndreBalt <90377920+IndreBalt@users.noreply.github.com>
2021-10-25 14:39:50 +0300

    first

---
+++ JS/add.js
@@ -0,0 +1,35 @@
+(function (){
+    const ul = document.querySelector('ul');
+    const btnShow = document.querySelector(".show");
+    const btnMix = document.querySelector(".mix");
+    const imagesBlock = document.querySelector(".images");
+
+    btnShow.addEventListener('click', showPic);
+
+    function showPic () {
+        for (i = 1; i <= 10; i++) {
+            ul.innerHTML += '<li><img src="IMG/download(' + [i] + ').jfif"></li>';
+        }
+        btnShow.style.display = "none";
+
+        const img = document.querySelectorAll(".images>ul>li>img");
+        const li = document.querySelectorAll('.images>ul>li');
+
+        for (let images of img) {
+            btnMix.addEventListener('click', mixPic);
+
+            function mixPic() {
+                images.src = 'http://localhost:63342/HTMl-layout/JS/DOM/uzduotis/IMG/download(' + Math.floor((Math.random() * li.length) + 1) + ').jfif';
+            }
+
+            images.addEventListener('click', changePic)
+
+            function changePic() {
+                images.src = 'http://localhost:63342/HTMl-layout/JS/DOM/uzduotis/IMG/download(7).jfif';
+            }
+
+        }
+
+    }
+
+})();
